5a2e5d9a8ca3f3079644bed50b50d28d6c469012
pyarelal <pyarelal@z1media.com>
2023-06-03 16:03:05 +0530

    Vue JS 2 Tutorial #4 - Data Binding On branch master Changes to be committed: 	modified:   app.js 	modified:   index.html

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var app = new Vue({
     el: '#app',
     data: {
         name: 'Pyare',
-        job: 'Software Development'
+        job: 'Software Development',
+        website: 'pyare.com',
+        website_tag: '<a href="www.gg.com">gg.com</a>'
     },
     methods: {
         greet: function(name='Pyare!') {
