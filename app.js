fbe20f609c44222b90c4d441b323445dd0cd6718
pyarelal <pyarelal@z1media.com>
2023-06-04 16:56:20 +0530

    Vue JS 2 Tutorial #12 - Looping with v-for On branch master Changes to be committed: 	modified:   app.js 	modified:   index.html

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var app = new Vue({
     el: '#app',
     data: {
-        success:false,
-        error: false
+        chars: ['char1', 'char2'],
+        ninjas: [
+            {name: 'Pyare', age: 30},
+            {name: 'Amit', age: 25},
+            {name: 'Prem', age: 32}
+        ]
     },
     methods: {
         
